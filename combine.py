db2a86cbea751c6a5349dca730f6e487bd73c938
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2020-01-13 18:22:38 -0500

    Add command line param for year

--- combine.py
+++ combine.py
@@ -1,6 +1,8 @@
 import csv
+import sys
 
-with open('countypres_2000-2016.csv','r') as in_file, open('presoutputfinal2012.csv','w') as out_file:
+year = sys.argv[1];
+with open('countypres_2000-2016.csv','r') as in_file, open('usPresElections' + year + '.csv','w') as out_file:
 	reader = csv.reader(in_file)
 	# get the headers and write them to outfile
 	h = next(reader, None) 
@@ -11,11 +13,11 @@ with open('countypres_2000-2016.csv','r') as in_file, open('presoutputfinal2012.
 	# Combine data with the same geoID into one row.
    	counties = dict()
    	for l in reader:
-   		year = l[0]
-   		if not year == '2012':
+   		rowYear = l[0]
+   		if not rowYear == year:
    			continue
    		fips = l[4];
-   		key = year +fips
+   		key = rowYear +fips
    		if not key in counties:
 			counties[key] = l[:5] + [l[9]];
 			if len(fips) == 4:
